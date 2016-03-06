e01a282a9047a85dadab293eb8d8d51204796b56
Alex DeMars <alexdemars@gmail.com>
2016-03-06 12:19:29 -0800

    create gulpfile, uglify app.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,14 @@ var gulp   = require('gulp'),
     gutil  = require('gulp-util'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat');
+
+uglifyScripts = function() {
+  gulp.src('./public/javascripts/app.js')
+    .pipe(uglify())
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./public/javascripts'))
+};
+
+gulp.task('default', function() {
+  uglifyScripts();
+});
